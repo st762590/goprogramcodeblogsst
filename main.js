09df9100e74ef78e22a8acb80a7536643daffa1e
st762590 <shivamtiwarirst@gmail.com>
2024-07-01 16:53:28 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const generatePDF = async (name) => {
+  const { PDFDocument, rgb } = PDFLib;
+  
+  const exBytes = await fetch("./Colorful Modern Geometric Certificate of Participation.pdf").then((res) => { 
+    return res.arrayBuffer();
+  });
+  
+const pdfDoc = await PDFDocument.load(exBytes)
+
+const uri = await pdfDoc.saveAsBase64({dataUri: true})
+
+document.querySelector("#mypdf").src = uri;
+};
+
+generatePDF()
