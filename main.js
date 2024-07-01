49b8d4fa4390257702568ca1feecfdcc668554db
st762590 <shivamtiwarirst@gmail.com>
2024-07-01 17:29:50 +0530

    update

--- main.js
+++ main.js
@@ -5,11 +5,32 @@ const generatePDF = async (name) => {
     return res.arrayBuffer();
   });
   
+  const exFont = await fetch("./Sanchez-Regular.ttf").then(res => {
+    return res.arrayBuffer()
+  })
+  
+
+  
+
+  
+  
 const pdfDoc = await PDFDocument.load(exBytes)
+
+  pdfDoc.resgisterFontkit(fontKit);
+  const myFont = await pdfDoc.embedFont(exFont);
+  
+  const pages = pdfDoc.getPages();
+  const firstPg = pages[0]
+  
+  firstPg.drawText(name,{
+    x: 300,
+    y: 270,
+    size: 58,
+  })
 
 const uri = await pdfDoc.saveAsBase64({dataUri: true})
 
 document.querySelector("#mypdf").src = uri;
 };
 
-generatePDF()
+generatePDF("Harsh Sharma")
